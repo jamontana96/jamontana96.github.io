3c18030ff2bdf992499fc05a721fb14ec86b6395
jamontana96 <126085464+jamontana96@users.noreply.github.com>
2026-06-10 18:16:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+/**
+ * Descarga la tarjeta como PNG
+ * @param {string} id
+ */
+function descargarImagen(id) {
+  const carta = document.getElementById(id);
+  let nombre_archivo = id + ".png";
+
+  html2canvas(carta, { scale: 8, allowTaint: true, useCORS: true }).then(
+    (canvas) => {
+      let base64img = canvas.toDataURL("image/png");
+      base64img.crossOrigin = "Anonymous";
+      let aux_hlink = document.createElement("a");
+      aux_hlink.setAttribute("href", base64img);
+      aux_hlink.setAttribute("download", nombre_archivo);
+      aux_hlink.click();
+      aux_hlink.remove();
+    },
+  );
+}
